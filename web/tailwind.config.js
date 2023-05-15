f93411567542206e994a79c0c0fdf0d28bc64d6b
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2023-05-15 17:29:19 -0300

    tailwind update

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,10 +1,21 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     './src/**/*.tsx'
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        dark: '#121214',
+        cloudy: '#2A2634',
+        rainy: '#1b1922'
+      },
+      backgroundImage: {
+        'rainbow-gradient': "linear-gradient(89.86deg, #9572FC 0%, #43E7AD 33.33%, #E1D55D 66.67%, #9572FC 100%)"
+      }
+    },
   },
   plugins: [],
 }
