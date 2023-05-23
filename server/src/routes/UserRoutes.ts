6ccc433025b752e3bc0eddd1784dda5730c54d9b
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2023-05-23 08:12:00 -0300

    update user route update

--- server/src/routes/UserRoutes.ts
+++ server/src/routes/UserRoutes.ts
@@ -6,6 +6,6 @@ const usersController = new UsersController()
 
 userRoutes.post("/register", usersController.register)
 userRoutes.post("/login", usersController.login)
-userRoutes.put("/update-user/:userId", usersController.updateUser)
+userRoutes.put("/update/:userId", usersController.updateUser)
 
 export { userRoutes }
