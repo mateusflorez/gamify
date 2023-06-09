015a10fae85e8ad8f5d0bdeb8e22e75bb37f393f
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2023-06-09 17:23:21 -0300

    linear progress visual update

--- web/src/components/LinearProgressWithLabel.tsx
+++ web/src/components/LinearProgressWithLabel.tsx
@@ -1,20 +1,23 @@
-import * as React from 'react';
 import LinearProgress, { LinearProgressProps } from '@mui/material/LinearProgress';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import { theme } from '../utils/utils'
+import { ThemeProvider } from '@mui/material';
 
 function LinearProgressWithLabel(props: LinearProgressProps & { value: number }) {
     return (
-        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <Box sx={{ width: '100%', mr: 1 }}>
-                <LinearProgress variant="determinate" {...props} />
+        <ThemeProvider theme={theme}>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <Box sx={{ width: '100%', mr: 1 }}>
+                    <LinearProgress variant="determinate" color="primary" {...props} />
+                </Box>
+                <Box sx={{ minWidth: 35 }}>
+                    <Typography variant="body2" color="text.secondary">{`${Math.round(
+                        props.value,
+                    )}%`}</Typography>
+                </Box>
             </Box>
-            <Box sx={{ minWidth: 35 }}>
-                <Typography variant="body2" color="text.secondary">{`${Math.round(
-                    props.value,
-                )}%`}</Typography>
-            </Box>
-        </Box>
+        </ThemeProvider>
     );
 }
 
